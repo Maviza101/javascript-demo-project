b45751e7bc531692d59cb656587974b89e4f88c4
Folusho <folusho@lanthanidesoft.com>
2017-03-10 16:47:03 +0100

    Used yet another approach for tests.

--- tests/tests.js
+++ tests/tests.js
@@ -80,3 +80,42 @@ describe("Temperature Conversion Functionality", function() {
     expect(myApp.convertTempFtoC(104)).to.equal(40);
   })
 })
+
+// Tests using should.
+// Note that we're using chai.should(), NOT chai.should
+var should = chai.should();
+describe("Arithmetic Functionality", function() {
+  it("should return 2 as average for 1, 2, 3", function() {
+    myApp.computeAverage(1, 2, 3).should.equal(2);
+  })
+  it("should return 5 as average for 3, 7, 5", function() {
+    myApp.computeAverage(3, 7, 5).should.equal(5);
+  })
+  it("should return 120 as factorial for 5", function() {
+    myApp.computeFactorial(5).should.equal(120);
+  })
+  it("should return 1 as factorial for 0", function() {
+    myApp.computeFactorial(0).should.equal(1);
+  })
+  it("should return 1 as factorial for 1", function() {
+    myApp.computeFactorial(1).should.equal(1);
+  })
+  it("should return an error message for -3", function() {
+    myApp.computeFactorial(-3).should.equal("Cannot find factorial of negative number");
+  })
+})
+
+describe("Temperature Conversion Functionality", function() {
+  it("should return 104 for Celcius value 40", function() {
+    myApp.convertTempCtoF(40).should.equal(104);
+  })
+  it("should return 140 for Celcius value 60", function() {
+    myApp.convertTempCtoF(60).should.equal(140);
+  })
+  it("should return 60 for Fahrenheit value 140", function() {
+    myApp.convertTempFtoC(140).should.equal(60);
+  })
+  it("should return 40 for Fahrenheit value 104", function() {
+    myApp.convertTempFtoC(104).should.equal(40);
+  })
+})
