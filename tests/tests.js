8c47909e7ec517f3259a9f535bb1aa524fdf42ff
Folusho <folusho@lanthanidesoft.com>
2017-03-10 16:23:14 +0100

    Improved code structure.

--- tests/tests.js
+++ tests/tests.js
@@ -29,6 +29,21 @@ describe("Arithmetic Functionality", function() {
   })
 })
 
+describe("Temperature Conversion Functionality", function() {
+  it("should return 104 for Celcius value 40", function() {
+    assert(myApp.convertTempCtoF(40) == 104);
+  })
+  it("should return 140 for Celcius value 60", function() {
+    assert(myApp.convertTempCtoF(60) == 140);
+  })
+  it("should return 60 for Fahrenheit value 140", function() {
+    assert(myApp.convertTempFtoC(140) == 60);
+  })
+  it("should return 40 for Fahrenheit value 104", function() {
+    assert(myApp.convertTempFtoC(104) == 40);
+  })
+})
+
 // Tests using expect.
 describe("Arithmetic Functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
@@ -63,21 +78,5 @@ describe("Temperature Conversion Functionality", function() {
   })
   it("should return 40 for Fahrenheit value 104", function() {
     expect(myApp.convertTempFtoC(104)).to.equal(40);
-  })
-})
-
-
-describe("Temperature Conversion Functionality", function() {
-  it("should return 104 for Celcius value 40", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
-  })
-  it("should return 140 for Celcius value 60", function() {
-    assert(myApp.convertTempCtoF(60) == 140);
-  })
-  it("should return 60 for Fahrenheit value 140", function() {
-    assert(myApp.convertTempFtoC(140) == 60);
-  })
-  it("should return 40 for Fahrenheit value 104", function() {
-    assert(myApp.convertTempFtoC(104) == 40);
   })
 })
